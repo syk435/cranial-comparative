e705cba6b630aa10e04c8c743975244aae1ee22f
syk435 <salemkarani@gmail.com>
2017-04-29 20:28:04 -0400

    removing legacy windows app from Master branch, preserving on Legacy branch

--- nativeWindowsCranial/CPD2/Running/RunningScript.m
+++
@@ -1,8 +0,0 @@
-%Script written in 20150205.
-%addpath('C:\Users\Dell\Documents\OCAF\TCH research 20160211\PlasticSurgeryJournal\CPD2\Running')
-%mex -setup
-patient_Registration('Patient1_decimated.txt','Patient_4_decimated95percent.txt','CPD_Patient3_to_Patient1COR.txt','CPD_Update_Patient3.txt');
-%patient_Registration('CPD_9016091PRE_Sim_7444.txt','CPD_9016091POST4_Sim_7252.txt','CPD_9016091COR.txt','CPD_9016091POST4_Update_7252.txt');
-%patient_Registration('CPD_3000415814PRE_Sim_8371.txt','CPD_3000415814POST1_Sim_8792.txt','CPD_3000415814COR.txt','CPD_3000415814POST1_Update_8792txt');
-%patient_Registration('CPD_3001425158PRE_Sim_6951.txt','CPD_3001425158POST_Sim_6903.txt','CPD_3001425158COR.txt','CPD_3001425158POST_Update_6903.txt');
-%patient_Registration('CPD_3001518168PRE_Sim_8971.txt','CPD_3001518168POST_Sim_8251.txt','CPD_3001518168COR.txt','CPD_3001518168POST_Update_8251.txt');
--- nativeWindowsCranial/vtkOBJWriter-master/Test/README.txt
+++
@@ -1,1 +0,0 @@
-The files in the parent directory must be copied to VTK/Rendering, VTK recompiled, then this project built.
